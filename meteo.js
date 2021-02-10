9c7ef2e643d1c47fd2dae69ba190021a57b2d49e
sudoCalvin <78729498+sudoCalvin@users.noreply.github.com>
2021-02-10 22:36:54 +0100

    Update meteo.js

--- meteo.js
+++ meteo.js
@@ -9,11 +9,12 @@ function showCity() {
     const div = document.createElement("div");
     div.classList.add("city");
     
-    document.querySelectorAll(".city").forEach(a=>a.style.display="none");
+    /* document.querySelectorAll(".city").forEach(a=>a.style.display="none"); */
 
     arrayVilles.forEach(function(i) {
         if(i==codeVille)
             div.style.display = "";
+            
             
             fetch(`https://api.openweathermap.org/data/2.5/weather?q=${codeVille}&appid=eb9f6ae73deae1d90596cc4450f1e973&units=metric`)
                 .then(response => response.json())
@@ -21,14 +22,20 @@ function showCity() {
                     const {main, name, sys, weather } = data;
                     //console.log(data.weather[0].description) // get état sky
                     const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
+                    
                     const markup = `
+                    <h2>${codeVille}</h2>
+                    
                     <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
                     <figure>
                     <img class="city-icon" src="${icon}" alt="${weather[0]["description"]}">
                     <figcaption>${weather[0]["description"]}</figcaption>
-                    </figure>`;
+                    </figure>
+                    `;
                     div.innerHTML = markup;
                     list.appendChild(div);
-        })
+                }
+        )
     })
+}
 }
